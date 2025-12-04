5492cb777cd3f87e8309d83f05918ddae8dad26a
sujal singh <143196432+SujalSingh9252@users.noreply.github.com>
2025-12-04 12:35:56 +0530

    Delete Frontend-Dev-js-dom directory

--- Frontend-Dev-js-dom/assignment7/4_Theme_Switcher/script.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const buttons = document.querySelectorAll('button[data-theme]');
-buttons.forEach(b=> b.addEventListener('click', ()=> {
-  const t = b.getAttribute('data-theme');
-  document.body.setAttribute('data-theme', t);
-}));
